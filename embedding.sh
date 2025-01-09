542611eed182214b8f70e0c69d6b12343c086744
Andrew_Xu <andrew.xu@monash.edu>
2025-01-10 09:44:48 +1100

    added embedding slurm script

---
+++ embedding.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --job-name=embedding_trial_run
+#SBATCH --output=slurm_outputs/dsm_embedding_%j.log
+#SBATCH --error=slurm_outputs/dsm_embedding_%j.err
+#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=32
+#SBATCH --mem=128G
+#SBATCH --ntasks=1
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=xu.a@wehi.edu.au
+
+module load python
+
+source .env/bin/activate
+python dsm_embedding_script.py
+deactivate
